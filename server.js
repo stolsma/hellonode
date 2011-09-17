bc601255aab6da1c60be8ffc641927eeb87975de
Sander Tolsma <github@tolsma.net>
2011-09-17 23:28:35 +0300

    createServer doesnt accept two listen. So create ipv4 and ipv6 server

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('hello, i know nodejitsu.')
+  res.write('hello, i know nodejitsu on ipv4.')
   res.end();
-}).listen(8000, '0.0.0.0').listen(8000, '::');
+}).listen(8000, '0.0.0.0').
 
 
-console.log('> http server has started on port 8000');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('hello, i know nodejitsu on ipv6.')
+  res.end();
+}).listen(8000, '::');
+
+
+console.log('> http server has started on port 8000 (ipv4 and ipv6)!');
