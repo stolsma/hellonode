52f70e6b14aa4d7658f174c6a25c79abe9f6a6e5
Sander Tolsma <github@tolsma.net>
2011-09-27 19:26:35 +0300

    Correct ipv4 and ipv6 interface binding

--- server.js
+++ server.js
@@ -2,16 +2,8 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('hello, i know nodejitsu on ipv4.')
+  res.write('hello, i know nodejitsu on ipv4 and ipv6.')
   res.end();
-}).listen(8000, '0.0.0.0');
-
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('hello, i know nodejitsu on ipv6.')
-  res.end();
-}).listen(8000, '2001:888:2000:47::4:1');
-
+}).listen(8000, '::0');
 
 console.log('> http server has started on port 8000 (ipv4 and ipv6)!');
