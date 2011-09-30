484e654017b67690131f17022a48de80e29a53fd
Sander Tolsma <github@tolsma.net>
2011-09-30 15:02:12 +0200

    Test environment after double spawn

---
+++ output.js
@@ -0,0 +1,2 @@
+console.log('This is output from output.js!!!!');
+console.log('output.js env: ', process.env);
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var http = require('http');
+var http = require('http'),
+	exec = require('child_process').exec;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -9,3 +10,14 @@ http.createServer(function (req, res) {
 console.log('> http server has started on port 8000 (ipv4 and ipv6)!');
 console.log('> arguments: ', process.argv);
 console.log('> env: ', process.env);
+console.log();
+
+
+child = exec('node /home/haibu_user_1/data/local/test/hellonode/output.js',
+  function (error, stdout, stderr) {
+    console.log('output.js stdout: ' + stdout);
+    console.log('output.jsstderr: ' + stderr);
+    if (error !== null) {
+      console.log('output.js exec error: ' + error);
+    }
+});
